7344317b0ce3425dc12227c579f4d0272422f34e
kvncampos <kvnelpdfw@hotmail.com>
2024-02-09 20:32:54 -0600

    first commit

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// ---------------------------------------------------------------------------------------------------------
+
+// /api/shorturl; Returns Original URL and shortcode for it. Need to first check if URL is Valid
+
+const dns = require('node:dns');
+const { error } = require('node:console');
+const options = {
+  family: 4,
+  // hints: dns.ADDRCONFIG | dns.V4MAPPED,
+};
+
+app.post('/api/:shorturl', (req, res) => {
+    dns.lookup(req.params.shorturl, options, (err, address, family) => {
+      if (err) {
+        return console.log(err)
+      }
+      console.log('address: %j family: IPv%s', address, family)
+      console.log(err)
+  });
+});
+
+// ---------------------------------------------------------------------------------------------------------
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
