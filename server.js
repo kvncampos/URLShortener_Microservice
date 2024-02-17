8034f14fbfe8491225cbdcf0c49bc90931f8738c
kvncampos <kvnelpdfw@hotmail.com>
2024-02-16 22:18:44 -0600

    cors enabled

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./DB/db');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/', routes);
